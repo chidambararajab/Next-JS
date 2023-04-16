3bef7c9bfd24790e7b622b093a8f37f4fe27baf4
chidambararaja.b <chidambararaja.b@accubits.com>
2023-04-16 22:03:15 +0530

    [Previous]

--- next-pre-rendering/pages/posts/[postId].js
+++ next-pre-rendering/pages/posts/[postId].js
@@ -1,10 +1,10 @@
-import { useRouter } from 'next/router'
+import { useRouter } from "next/router";
 
 function Post({ post }) {
-  const router = useRouter()
+  const router = useRouter();
 
   if (router.isFallback) {
-    return <div>Loading...</div>
+    return <div>Loading...</div>;
   }
 
   return (
@@ -14,30 +14,30 @@ function Post({ post }) {
       </h2>
       <p>{post.body}</p>
     </>
-  )
+  );
 }
 
-export default Post
+export default Post;
 
 export async function getStaticProps(context) {
-  const { params } = context
+  const { params } = context;
   const response = await fetch(
     `https://jsonplaceholder.typicode.com/posts/${params.postId}`
-  )
-  const data = await response.json()
+  );
+  const data = await response.json();
 
   if (!data.id) {
     return {
-      notFound: true
-    }
+      notFound: true,
+    };
   }
 
-  console.log(`Generating page for /posts/${params.postId}`)
+  console.log(`Generating page for /posts/${params.postId}`);
   return {
     props: {
-      post: data
-    }
-  }
+      post: data,
+    },
+  };
 }
 
 export async function getStaticPaths() {
@@ -51,10 +51,10 @@ export async function getStaticPaths() {
 
   return {
     paths: [
-      { params: { postId: '1' } },
-      { params: { postId: '2' } },
-      { params: { postId: '3' } }
+      { params: { postId: "1" } },
+      { params: { postId: "2" } },
+      { params: { postId: "3" } },
     ],
-    fallback: true
-  }
+    fallback: true,
+  };
 }
